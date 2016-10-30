bc695250bc7d519efab0027efafdc367fa6629f3
Owen Voke <owzie123@gmail.com>
2016-10-30 17:57:38 +0000

    Update funcs.php
    Realigned :P

--- backend/funcs.php
+++ backend/funcs.php
@@ -8,7 +8,7 @@ class Functions
     public static function conn()
     {
         /* MySQL VARIABLES */
-            $db_serv = 'localhost';
+        $db_serv = 'localhost';
         $db_user = 'root';
         $db_pass = 'root';
         $db_name = 'kat_status';
